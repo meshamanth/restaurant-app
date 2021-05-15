7fb4d431adeb855f0e8ef3a4b8373a734710c341
meshamanth <shamanth.kr@gmail.com>
2021-05-15 11:37:42 +0530

    second add

---
+++ src/components/SearchBar.js
@@ -0,0 +1,44 @@
+import React from "react";
+import { StyleSheet, Text, View, TextInput } from "react-native";
+import { Feather } from "@expo/vector-icons";
+
+const SearchBar = ({ term, onTermChange, onTermSubmit }) => {
+  return (
+    <View style={styles.backgroundStyle}>
+      <Feather name="search" style={styles.iconStyle} />
+      <TextInput
+        style={styles.inputStyle}
+        placeholder="Search"
+        value={term}
+        onChangeText={onTermChange}
+        autoCapitalize="none"
+        autoComplete={false}
+        onEndEditing={onTermSubmit}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  backgroundStyle: {
+    marginTop: 10,
+    marginBottom: 10,
+    backgroundColor: "#F0EEEE",
+    height: 50,
+    borderRadius: 5,
+    marginHorizontal: 15,
+    flexDirection: "row",
+  },
+  iconStyle: {
+    fontSize: 35,
+    alignSelf: "center",
+    color: "black",
+    marginHorizontal: 15,
+  },
+  inputStyle: {
+    flex: 1,
+    fontSize: 18,
+  },
+});
+
+export default SearchBar;
---
+++ src/screen/SearchScreen.js
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+import { Text, View, ScrollView } from "react-native";
+
+import SearchBar from "../components/SearchBar";
+import useResults from "../hooks/useResults";
+import ResultsList from "../components/ResultsList";
+
+const SearchScreen = () => {
+  const [term, setTerm] = useState("");
+  const [searchApi, results, errorMessage] = useResults();
+
+  const filterResultsByPrice = (price) => {
+    // price === $ ||$$||$$$
+    return results.filter((result) => {
+      return result.price === price;
+    });
+  };
+
+  return (
+    <>
+      <SearchBar
+        term={term}
+        onTermChange={setTerm}
+        onTermSubmit={() => searchApi(term)}
+      />
+      <ScrollView>
+        <ResultsList
+          results={filterResultsByPrice("$")}
+          title="Cost Effective"
+        />
+        <ResultsList results={filterResultsByPrice("$$")} title="Bit Pricier" />
+        <ResultsList
+          results={filterResultsByPrice("$$$")}
+          title="Big Spender"
+        />
+      </ScrollView>
+    </>
+  );
+};
+
+export default SearchScreen;
